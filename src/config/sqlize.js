01dba13c4f573fbb0f47b2d42fec59138389a252
M. Allulu <mohammed.allulu@live.com>
2022-09-23 17:17:14 +0300

    updated local environment variables

--- src/config/sqlize.js
+++ src/config/sqlize.js
@@ -1,13 +1,15 @@
 const Sqlize = require("sequelize");
 require('dotenv').config();
 
+console.log(process.env.DB_NAME);
+
 module.exports = new Sqlize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PASSWORD, 
     {
         host: process.env.DB_HOST,
-        port: '3307',
+        port: process.env.DB_PORT,
         dialect: 'mysql',
         logging: false
     }
